48d1919d5a0bcb9debd5efe2d7a86d02595b8239
nitin833 <nitin.vempati@gmail.com>
2025-02-21 10:56:10 +0530

    newly seperated js file from admin dashboard ad style sheet also

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -1,9 +1,9 @@
-const express = require(express());
-
+const express = require('express');
+const path = require('path');
 const router = express.Router();
 
-
-router.get('dashboard',(req,res)=>{
+console.log("vfb");
+router.get('/dashboard',(req,res)=>{
 
 res.sendFile(path.join(__dirname,'..','Views','index.html'));
 
