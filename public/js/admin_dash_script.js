f5e7e8b78dda9e352a2440024e4e7ded332764c9
nitin833 <nitin.vempati@gmail.com>
2025-02-25 16:04:02 +0530

    changed some names of files

--- public/js/admin_dash_script.js
+++ public/js/admin_dash_script.js
@@ -12,28 +12,43 @@ let admin_dash_data = {
   let btn = document.querySelector("#naddashbtn");
   
   let creatediv = () => {
-    
-      let rev = document.createElement("div");
-      rev.classList.add("nadrestlist");
-  
-      let h1 = document.createElement('h1');
-      h1.style.backgroundColor = "blue";
-      h1.innerHTML="this is restaurent";
-      rev.appendChild(h1);
-    
-      let innerdiv = document.createElement('div');
-    
-      let date = document.createElement('div');
-      date.innerHTML = 48;
-      let con = document.createElement('con');
-      con.innerHTML = 48;
-    
-      innerdiv.appendChild(date);
-      innerdiv.appendChild(con);
-    
-      rev.appendChild(innerdiv);
-      return rev;
-  }
+    let rev = document.createElement("div");
+    rev.classList.add("naddash-container");
+
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add("naddash-details");
+
+    // Name
+    let nameDiv = document.createElement('div');
+    nameDiv.classList.add("naddash-name");
+    nameDiv.innerHTML = `<strong>Name:</strong> John Doe`;
+
+    // Location
+    let locationDiv = document.createElement('div');
+    locationDiv.classList.add("naddash-location");
+    locationDiv.innerHTML = `<strong>Location:</strong> New York`;
+
+    // Payment Amount
+    let paymentDiv = document.createElement('div');
+    paymentDiv.classList.add("naddash-payment");
+    paymentDiv.innerHTML = `<strong>Payment Amount:</strong> $100`;
+
+    // Date Joined
+    let dateDiv = document.createElement('div');
+    dateDiv.classList.add("naddash-date");
+    dateDiv.innerHTML = `<strong>Date Joined:</strong> 24-Feb-2025`;
+
+    // Append elements
+    innerDiv.appendChild(nameDiv);
+    innerDiv.appendChild(locationDiv);
+    innerDiv.appendChild(paymentDiv);
+    innerDiv.appendChild(dateDiv);
+
+    rev.appendChild(innerDiv);
+    return rev;
+}
+
+
   
   btn.addEventListener('click', () => {
       let di = document.querySelector(".nad.restaurents_list");
