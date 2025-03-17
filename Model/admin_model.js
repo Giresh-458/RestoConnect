973c58da24f80d1081d5bcc6be4f969693fbd5f5
nitin833 <nitin.vempati@gmail.com>
2025-03-17 21:44:02 +0530

    nitin

--- Model/admin_model.js
+++ Model/admin_model.js
@@ -1,19 +1,55 @@
-const { name } = require('ejs');
 
+const sqlite3 = require('sqlite3').verbose();
 
+let active_user_count = 34;
+let total_user_count = 200;
 
+let db = new sqlite3.Database(":memory:", (err) => {
+    if (err) {
+        console.error("Error opening database:", err.message);
+    } else {
+        db.run(
+            `CREATE TABLE userstats (
+                active_user_count INTEGER,
+                total_user_count INTEGER
+            )`,
+            (err) => {
+                if (err) console.error("Error creating table:", err.message);
+                else {
+                    
+                    db.run(
+                        `INSERT INTO userstats (active_user_count, total_user_count) VALUES (?, ?)`,
+                        [active_user_count, total_user_count],
+                        (err) => {
+                            // console.log("hello");
+                            if (err) console.error("Error inserting data:", err.message);
+                        }
+                    );
+                }
+            }
+        );
+    }
+});
 
+const getDb = (cb) => {
+    if (!db) {
+        console.error("Database not initialized!");
+        return;
+    }
+    cb(db);
+};
 
-     active_user_count = 34;
-     total_user_count = 200;
-    const restaurants_list = require('../Model/Restaurents_model').restaurants;
+const restaurants_list = require('../Model/Restaurents_model').restaurants.map(r1 => ({
+    name: r1.name,
+    location: r1.location,
+    amount: r1.amount,
+    date: r1.date
+}));
 
-
-
-exports.active_user_count=active_user_count;
-exports.total_user_count=total_user_count;
-
-exports.restaurants_list = restaurants_list.map(r1=> ({name:r1.name,location:r1.location,amount:r1.amount,date:r1.date}));
-
-
-
+// Export properly using `module.exports`
+module.exports = {
+    active_user_count,
+    total_user_count,
+    restaurants_list,
+    getDb
+};
