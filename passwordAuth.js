30d4c2e20547f5d215c8e2f730773622711ba88a
nitin833 <nitin.vempati@gmail.com>
2025-03-18 08:46:14 +0530

    nitin

--- passwordAuth.js
+++ passwordAuth.js
@@ -13,6 +13,7 @@ let validate = (req,res,next)=>{
 let {username:username,password:password,fullName:fullName} = req.body;
 
 
+console.log(fullName);
 if(fullName){
     customer.push(new Person(username,'/images/benjamin-chambon-vRu-Bs27E2M-unsplash.jpg'));
     users.push(new User(username,'customer',null,password.toString().trim()));
@@ -24,7 +25,8 @@ if(fullName){
 
 let user = users.find(r => r.username == username);
 
-if(user?.password || null == password.toString().trim()){
+if((user?.password || null) == (password.toString().trim())){
+   
     next();
 }
 else{
